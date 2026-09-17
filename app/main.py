fc3c5bd30e36b72a932d81a70e79443290cdce30
gattu-git <github.priyanshu.9090@gmail.com>
2026-09-17 15:53:51 +0530

    add cors

--- app/main.py
+++ app/main.py
@@ -18,11 +18,12 @@ origins = [
     "http://localhost:8000",  # Default Create React App port
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
+    "https://algorithmic-crypto-strategy-backtesting.onrender.com",
     "ws://127.0.0.1:8000"
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
